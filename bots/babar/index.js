4985190610421a9a547e78a9d5233274d27927e4
Holderness <andersonholderness@gmail.com>
2015-10-24 11:22:11 -0400

    add Babar responses

--- bots/babar/index.js
+++ bots/babar/index.js
@@ -1,13 +1,14 @@
 
 'use strict';
 
-let Bot = require('slackbots');
+let _ = require('underscore'),
+Bot = require('slackbots');
 
 module.exports = () => {
 
   let babar = new Bot({
-      token: process.env.BABAR_KEY,
-      name: 'babar'
+    token: process.env.BABAR_KEY,
+    name: 'babar',
   });
 
   babar.params = {
@@ -20,14 +21,49 @@ module.exports = () => {
   
   babar.on('message', (data) => {
     console.log('data: ', data);
-    console.log('babar params: ', babar.params);
-    if (data.type === 'message' && data.username !== 'babar' && data.text === 'Want some water Babar?') {
-      babar.postMessageToChannel('general', 'Babar only drinks the blood of enemies', babar.params);
-    }
-    if (data.type === 'message' && data.username !== 'babar' && data.text === 'Babar, what are you doing tomorrow?') {
-      babar.postMessageToChannel('general', 'Well, I\'m going to polka dancing with Charlene, my sex-ambiguous escort, and then later I\'m eating a family-sized bag of Combos and watching Maury', babar.params);
+    if (data.username === 'babar') {return;}
+    if (data.type === 'message') {
+      initResponses(data.text);
     }
   });
+
+  let initResponses = (dataText) => {
+    hello(dataText);
+    water(dataText);
+    tomorrow(dataText);
+  };
+
+  let parseText = (text, key) => {
+    return _.contains(text.split(' '), key);
+  };
+
+  let response = (responseText) => {
+    babar.postMessageToChannel('general', responseText, babar.params);
+  };
+
+  let hello = (text) => {
+    console.log('text: ', text);
+    console.log('parsettext: ', parseText(text, 'Babar'));
+    if (parseText(text, 'Babar')) {
+      response('WHAT WHAT IS IT LEAVE ME ALONE');
+    }
+  };
+
+  let water = (text) => {
+    if (parseText(text, 'water')) {
+      response('Babar only drinks the blood of enemies');
+    }
+  };
+
+  let tomorrow = (text) => {
+    if (parseText(text, 'tomorrow')) {
+      response('Well, I\'m going to polka dancing with Charlene, my sex-ambiguous escort, and then later I\'m eating a family-sized bag of Combos and watching Maury');
+    }
+  };
+
+
+
+
 
 
 
