bba5a30776dd104e5bead59da8b17900f57d8214
Evan Turner <evturn@gmail.com>
2015-10-26 17:19:29 -0400

    Add Cron module

--- bots/clang/index.js
+++ bots/clang/index.js
@@ -1,18 +1,23 @@
 'use strict';
-
-let Bot = require('slackbots');
+const Bot = require('slackbots');
+const CronJob = require('cron').CronJob;
 
 module.exports = () => {
 
-  let clang = new Bot({
+  const clang = new Bot({
       token: process.env.CLANG_KEY,
-      name: 'calng'
+      name: 'clang'
   });
 
   clang.on('start', () => {
     let params = {
-        icon_emoji: ':neutral_face:'
+        icon_url: 'http://geekdad.com/images_blogs/photos/uncategorized/2007/06/08/cb2.jpg'
     };
-    // clang.postMessageToChannel('general', 'Clanging and banging.', params);
+    clang.postMessageToChannel('roughhouse', 'Clanging and banging.', params);
   });
+
+  new CronJob('00 00 * * * *', () => {
+    clang.postMessageToChannel('general', 'Clang. Every hour, on the hour.', params);
+  }, null, true);
+
 }();
