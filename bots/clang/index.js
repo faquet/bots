e122d332149e5d81130587889d8dacd16b1c7b59
Holderness <andersonholderness@gmail.com>
2015-10-31 18:57:04 -0400

    add turn clang back on

--- bots/clang/index.js
+++ bots/clang/index.js
@@ -1,18 +1,18 @@
-// 'use strict';
-// const Bot = require('../../src/bot');
-// const scheduler = require('./lib/scheduler');
+'use strict';
+const Bot = require('../../src/bot');
+const scheduler = require('./lib/scheduler');
 
-// const clang = new Bot({
-//   token      : process.env.CLANG_KEY,
-//   name       : 'clang',
-//   username   : 'clang',
-//   real_name  : '__clang__',
-//   icon_url   : 'http://geekdad.com/images_blogs/photos/uncategorized/2007/06/08/cb2.jpg'
-// });
+const clang = new Bot({
+  token      : process.env.CLANG_KEY,
+  name       : 'clang',
+  username   : 'clang',
+  real_name  : '__clang__',
+  icon_url   : 'http://geekdad.com/images_blogs/photos/uncategorized/2007/06/08/cb2.jpg'
+});
 
-// clang.on('start', () => {
-//   clang.post('channel', 'roughhouse', 'I work now. I work for UPS.');
-//   scheduler(clang);
-// });
+clang.on('start', () => {
+  clang.post('channel', 'roughhouse', 'I work now. I work for UPS.');
+  scheduler(clang);
+});
 
-// module.exports = clang;
+module.exports = clang;
