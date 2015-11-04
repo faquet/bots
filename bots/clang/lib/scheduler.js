c5bed25f061b994721ab0532603e2caa37a22622
Evan Turner <evturn@gmail.com>
2015-11-03 22:32:33 -0500

    Remove lib from clang directory

--- bots/clang/lib/scheduler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const CronJob = require('cron').CronJob;
-
-module.exports = (bot, time, text) => {
-  return new CronJob(time, () => {
-    bot.post('channel', 'roughhouse', text);
-  }, null, true);
-};
