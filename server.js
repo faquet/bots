af4748fe6a96053fecfe21e3fcd70d4986dad5ed
Evan Turner <evturn@gmail.com>
2015-12-04 16:27:49 -0500

    Add comments to describe initialization

--- server.js
+++ server.js
@@ -5,8 +5,29 @@ const app = express();
 const mongoose = require('./bots/babar/config/mongoose');
 const db = mongoose();
 
+/**
+ *
+ * Application
+ * @params {object} - Object describing a bot's properties
+ *
+ */
+
 const SLACKBOT = require('./src').turn;
+
+/**
+ *
+ * Bots properties
+ *
+ */
+
 const bots = require('./bots');
+
+
+/**
+ *
+ * Bot Instances
+ *
+ */
 
 const clang = SLACKBOT(bots.clang);
 const borf = SLACKBOT(bots.borf);
